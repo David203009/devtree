de81b4cda2a994ab1899589b158c887109840001
David203009 <teranjegre@gmail.com>
2025-03-14 18:20:06 +0000

    encrypt password, add unique handle and validations with express validator

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,19 +1,50 @@
 import { Request, Response } from "express";
+
+//for validations
+import { validationResult } from 'express-validator';
+
+// slug for create user names without spaces and upper
+import slug from 'slug'
 import User from "../models/User";
+import { hashPassword } from '../utils/auth'
 
 export const createAccount = async (req: Request, res: Response) => {
     try {
-        const {email} = req.body;
+
+        //manage errors
+        let errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+          res.status(400).send({errors: errors.array()});
+          return;
+        }
+
+        const {email, password} = req.body;
+    
         const userExists = await User.findOne({email});
 
         if (userExists) {
-            return res.send({message: 'User already exists'});
+            res.status(409).send({message: 'User already exists'});
+            return;
+        }
+
+        const handle = slug(req.body.handle, '');
+        const handleExists = await User.findOne({handle});
+
+        if(handleExists){
+            res.status(409).send({message: 'Handle already exists'});
+            return;
         }
 
         const user = new User(req.body);
+        user.password = await hashPassword(user.password);
+        user.handle = handle;
+
         await user.save();
-        res.send({message: 'User created'});
+        res.status(200).send({message: 'User created'});
+        return;
     } catch (error) {
-        res.send({message: 'Error creating user'});
+        res.status(500).send({message: 'Error creating user', error: error});
+        return;  
     }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
+
+//for validate input form
+import { body } from 'express-validator';
 import { createAccount } from "./handlers/index";
 
 const router = Router();
 
 /** Autentication and registries */
-router.post("/auth/register", createAccount);
+router.post("/auth/register", 
+    body('handle').notEmpty(),
+    createAccount);
 
 
 export default router;
