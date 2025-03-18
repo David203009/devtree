dd8f42679d68e5946df91fc88271d39298079853
David203009 <77461961+David203009@users.noreply.github.com>
2025-03-18 16:50:05 -0400

    autenticando usuarios

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -4,9 +4,9 @@ import { Request, Response } from "express";
 import { validationResult } from 'express-validator';
 
 // slug for create user names without spaces and upper
-import slug from 'slug'
+import slugify from 'slugify'
 import User from "../models/User";
-import { hashPassword } from '../utils/auth'
+import { hashPassword, checkPassword } from '../utils/auth'
 
 export const createAccount = async (req: Request, res: Response) => {
     try {
@@ -28,7 +28,7 @@ export const createAccount = async (req: Request, res: Response) => {
             return;
         }
 
-        const handle = slug(req.body.handle, '');
+        const handle = slugify(req.body.handle, '');
         const handleExists = await User.findOne({handle});
 
         if(handleExists){
@@ -47,4 +47,28 @@ export const createAccount = async (req: Request, res: Response) => {
         res.status(500).send({message: 'Error creating user', error: error});
         return;  
     }
+}
+
+export const login = async (req: Request, res: Response) => {
+    let errors =  validationResult(req);
+    if(!errors.isEmpty()){
+        res.status(400).send({errors: errors.array()});
+        return;
+    }
+
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email});
+    if(!user){
+        res.status(404).send({error: 'El usuario no existe'})
+    }
+
+    const isPassCorect = checkPassword(password, user.password)
+    if(!isPassCorect){
+        res.status(401).send({error: 'Password incorrecto'})
+        return;
+    }
+
+    res.send('Autenticado.....')
+
 }
--- src/router.ts
+++ src/router.ts
@@ -2,14 +2,24 @@ import { Router } from "express";
 
 //for validate input form
 import { body } from 'express-validator';
-import { createAccount } from "./handlers/index";
+import { createAccount, login } from "./handlers/index";
 
 const router = Router();
 
 /** Autentication and registries */
 router.post("/auth/register", 
-    body('handle').notEmpty(),
-    createAccount);
+    body('handle').notEmpty().withMessage("El handle no puede ir vacio"),
+    body('name').notEmpty().withMessage("El nombre no puede ir vacio"),
+    body('email').isEmail().withMessage("El email no es valido"),
+    body('password').isLength({min: 8}).withMessage("El password no puede ser menor que 8 caracteres"),
+    createAccount
+);
+
+router.post('/auth/login', 
+    body('email').isEmail().withMessage("El email no es valido"),
+    body('password').notEmpty().withMessage("El password es obligatorio"),
+    login
+);
 
 
 export default router;
